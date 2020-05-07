ac973f58b6c60632a945036b5b3f89508ed62a62
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2020-05-07 00:47:06 +0000

    adding college to student, format index.html

--- student/models.py
+++ student/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.utils import timezone
+from college.models import College
 
 class Student(models.Model):
    name=models.CharField(max_length=500)
    mobile=models.CharField(max_length=20)
    email=models.CharField(max_length=100)
    date=models.DateTimeField(default=timezone.now)
+   college = models.ForeignKey(College, on_delete=models.CASCADE)
 
    def __str__(self):
        return self.name
